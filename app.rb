164003ee325b2d21de220b029ad79626fab1e6b3
Fizal Sarif <fizal.sarif@gmail.com>
2018-02-17 17:47:41 -0500

    initial

---
+++ app.rb
@@ -0,0 +1,49 @@
+# Based on http://www.jonathanleighton.com/articles/2011/awesome-active-record-bug-reports/ 
+
+# Run this script with `$ pry app.rb`
+require 'sqlite3'
+require 'active_record'
+
+# Use `binding.pry` anywhere in this script for easy debugging
+require 'pry'
+
+# Connect to a sqlite3 database
+# If you feel like you need to reset it, simply delete the file sqlite makes
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: 'carson.sqlite'
+)
+
+# Define a minimal database schema
+# Everytime the script is run it clears the databse, this 
+# is okay while working on the carson's request.
+ActiveRecord::Schema.define do
+  create_table :cars, force: true do |t|
+    t.string :name
+    t.string :color
+  end
+
+  # Create more tables here
+
+
+end
+
+# Define the models and relationships
+class Car < ActiveRecord::Base
+
+end
+
+class Customer < ActiveRecord::Base
+ 
+end
+
+class Invoice < ActiveRecord::Base
+
+end
+
+# Create a few records...
+# or import that CSV and create the appropriate records off of it. 
+
+
+# to let you use the terminal to CRUD the database. 
+binding.pry
