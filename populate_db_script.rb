7527b9ce34f8e5c9693627ffee9c715f91c6d65d
George Dagher <bongostar41@gmail.com>
2018-05-25 16:31:50 -0400

    georges updates

--- populate_db_script.rb
+++ populate_db_script.rb
@@ -1,22 +1,5 @@
-# Based on http://www.jonathanleighton.com/articles/2011/awesome-active-record-bug-reports/ 
+require './models'
 
-
-
-# If you feel like you need to reset it, simply delete the file sqlite makes
-
-
-# Define the models and relationships
-class Car < ActiveRecord::Base
-
-end
-
-class Customer < ActiveRecord::Base
- 
-end
-
-class Transaction < ActiveRecord::Base
-
-end
 
 # Everytime the script is run it clears the database, this 
 # is okay while working on carson's request.
@@ -28,4 +11,4 @@ Transaction.destroy_all
 # or import that CSV and create the appropriate records off of it. 
 
 
-# to let you use the terminal to CRUD the database. 
+# to let you use the terminal to CRUD the database.
